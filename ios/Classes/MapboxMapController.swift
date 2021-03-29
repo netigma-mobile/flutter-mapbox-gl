f8ecce2edd051768a674e43d963369e8035eeb2e
emre_netcad <emre.yalcin@netcad.com.tr>
2021-03-29 16:12:08 +0300

    fix ios draw

--- ios/Classes/MapboxMapController.swift
+++ ios/Classes/MapboxMapController.swift
@@ -472,19 +472,6 @@ class MapboxMapController: NSObject, FlutterPlatformView, MGLMapViewDelegate, Ma
         let tapPoint: CGPoint = sender.location(in: mapView)
         let tapCoordinate: CLLocationCoordinate2D = mapView.convert(tapPoint, toCoordinateFrom: mapView)
         print("You tapped at: \(tapCoordinate.latitude), \(tapCoordinate.longitude)")
-         
-        // Create an array of coordinates for our polyline, starting at the center of the map and ending at the tap coordinate.
-        var coordinates: [CLLocationCoordinate2D] = [mapView.centerCoordinate, tapCoordinate]
-         
-        // Remove any existing polyline(s) from the map.
-        if mapView.annotations?.count != nil, let existingAnnotations = mapView.annotations {
-        mapView.removeAnnotations(existingAnnotations)
-        }
-         
-        // Add a polyline with the new coordinates.
-        let polyline = MGLPolyline(coordinates: &coordinates, count: UInt(coordinates.count))
-        mapView.addAnnotation(polyline)
-        
         
         // Get the CGPoint where the user tapped.
         //let point = sender.location(in: mapView)
